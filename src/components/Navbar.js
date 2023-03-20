cbc0c290b361467503d83631e70d81ae463070fe
abraham <abrahamsuarezolay@gmail.com>
2023-03-20 11:33:08 +0000

    Added dynamic styling in navbar when scrolling down the site

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,13 +14,11 @@ const Navbar = () => {
         }
     }
 
-    window.addEventListener('scroll', changeColor)
-
-    //{color ? "navbar navbar-bg" : "navbar"}
+    window.addEventListener("scroll", changeColor)
 
     return(
-        
-            <div className= "navbar fixed-top">
+            <div className="fixed-top ms-1 me-1">
+            <div className={color ? "navbar navbar-bg" : "navbar"}>
                 <ul className="navbar-list">
                     <li><a href="#inicio" className="rootMargin: 20px">INICIO</a></li>
                     <li><a href="#nosotros" className="rootMargin: 20px">NOSOTROS</a></li>
@@ -28,6 +26,7 @@ const Navbar = () => {
                     <li><a href="#galeria" className="rootMargin: 20px">GALERÍA</a></li>
                     <li><a href="#contacto" className="rootMargin: 20px">CONTACTO</a></li>
                 </ul>
+            </div>
             </div>
         )
 }
